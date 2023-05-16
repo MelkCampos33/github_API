e5b691e929aaa30572104a286d1da6081e4b8a2a
Melkcampos <melquisedequecampos.trabalho@gmail.com>
2023-05-16 18:38:24 -0300

    api

---
+++ code.js
@@ -0,0 +1,46 @@
+const searchGithub = async () => {
+    const username = document.getElementById('searchInput')
+
+    const response = await fetch(`https://api.gitgub.com/users/${username}`)
+    const detailsContainer = document.querySelector('.details')
+    const data = await response.json() 
+
+    if(response.ok) {
+
+        detailsContainer.style.display = 'flex'
+
+        document.getElementById('result').innerHTML = `
+
+            <div class='profile'>
+                <div class='profile-image'>
+                <img src='${data.avatar_url}' />
+            </div>
+
+            <div class='profile-details'>
+                <h2 class='name'>${data.name || data.login}</h2>
+                <p class='username'>@${data.login}</p>
+            </div>
+
+            <p class='bio'>@${data.bio || 'Account doesn\'t have a bio'}</p>
+
+            <div class='stats'>
+                <div>
+                    <div class='stats-name'> Public Repos </div>
+                    <div class='stats-value'>${data.public_repos}</div>
+            </div>
+
+            <div>
+            <div class='stats-name'> Following </div>
+            <div class='stats-value'>${data.following}</div>
+        `
+
+    }
+}
+
+
+
+
+
+
+
+
